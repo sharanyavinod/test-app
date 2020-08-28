b380d7060bdeaa015fcccbfa879d550ec4e7c632
sharanyavinod <sharanyavinod90@gmail.com>
2020-08-28 14:13:28 +0200

    fix: package name updated
    Package has been renamed to avoid duplicate issue
    
    BREAKING CHANGE: Rename of package

--- src/components/Excerpt.js
+++ src/components/Excerpt.js
@@ -16,6 +16,15 @@ const Excerpt = () => (
             "I don't know, I didn't listen.”
         </p>
         <p>
+            “A towel, [The Hitchhiker's Guide to the Galaxy] says, is about the most massively useful thing an interstellar hitchhiker can have.
+            Partly it has great practical value. You can wrap it around you for warmth as you bound across the cold moons of Jaglan Beta;
+            you can lie on it on the brilliant marble-sanded beaches of Santraginus V, inhaling the heady sea vapors; you can sleep under it
+            beneath the stars which shine so redly on the desert world of Kakrafoon; use it to sail a miniraft down the slow heavy River Moth;
+            wet it for use in hand-to-hand-combat; wrap it round your head to ward off noxious fumes or avoid the gaze of the Ravenous Bugblatter
+            Beast of Traal (such a mind-boggingly stupid animal, it assumes that if you can't see it, it can't see you); you can wave your towel
+            in emergencies as a distress signal, and of course dry yourself off with it if it still seems to be clean enough.”
+        </p>
+        <p>
             Now it is such a bizarrely improbable coincidence that anything so mind-bogglingly useful could have evolved purely by chance that some thinkers have chosen to see it as the final and clinching proof of the non-existence of God.
             The argument goes something like this: "I refuse to prove that I exist,'" says God, "for proof denies faith, and without faith I am nothing."
             "But," says Man, "The Babel fish is a dead giveaway, isn't it? It could not have evolved by chance. It proves you exist, and so therefore, by your own arguments, you don't. QED."
